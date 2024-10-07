df8bd00940341772d8c7e45f58f31e8ce36d6509
Hunter Baker <62899372+literally-anything@users.noreply.github.com>
2024-10-07 14:09:29 -0400

    Update supported platforms in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "Fast-DDS-Prebuild",
+    platforms: [
+    	.macOS(.v10_13),
+    	.iOS(.v12),
+    	.visionOS(.v1)
+    ],
     products: [
         .library(name: "Fast-DDS", targets: ["Fast-DDS"])
     ],
